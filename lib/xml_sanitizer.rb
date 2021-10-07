34b088b3fd6934b6e6fe4737b10c4ff7289bc004
Amr El Bakry <amrr@hey.com>
2021-10-07 22:08:40 +0200

    Add sanitize, sanitize_docment and sanitize_fragment

--- lib/xml_sanitizer.rb
+++ lib/xml_sanitizer.rb
@@ -1,8 +1,25 @@
 # frozen_string_literal: true
 
+require 'loofah'
 require_relative 'xml_sanitizer/version'
+require_relative 'xml_sanitizer/default_permit_scrubber'
 
 module XmlSanitizer
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    def sanitize(xml:, scrubber: DefaultPermitScrubber.new)
+      xml_document = Loofah.xml_document(xml)
+      
+      xml_document.scrub!(scrubber)
+      xml_document.to_xml
+    end
+
+    alias sanitize_document sanitize
+
+    def sanitize_fragment(xml:, scrubber: DefaultPermitScrubber.new)
+      xml_fragment = Loofah.xml_fragment(xml)
+      
+      xml_fragment.scrub!(scrubber)
+      xml_fragment.to_xml
+    end
+  end
 end
