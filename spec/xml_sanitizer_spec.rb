ede1166f6c1b1f18bbe5d9b985d2dcc6a4c70fd1
Amr El Bakry <amrr@hey.com>
2021-10-07 22:18:07 +0200

    Remove useless spec

--- spec/xml_sanitizer_spec.rb
+++ spec/xml_sanitizer_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe XmlSanitizer do
   it 'has a version number' do
     expect(XmlSanitizer::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
